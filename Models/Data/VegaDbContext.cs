2572e8647676c02e340387739ac2137aa6146894
L666F <fiorani.luca1998@gmail.com>
2018-05-24 12:24:13 +0200

    Creating DbContext

---
+++ Models/Data/VegaDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vega.Models.Data
+{
+    public class VegaDbContext : DbContext
+    {
+        public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)
+        {}
+
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<CarModel> CarModels { get; set; }
+    }
+}
