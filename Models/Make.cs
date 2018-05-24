bf37dc711bc4712514f142271a89581f6ad050f3
L666F <fiorani.luca1998@gmail.com>
2018-05-24 12:24:24 +0200

    Domain Models

---
+++ Models/Make.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vega.Models
+{
+    public class Make
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public List<CarModel> Models { get; set; }
+
+        public Make() => Models = new List<CarModel>();
+    }
+}
