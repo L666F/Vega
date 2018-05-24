dba921289f0b91c5680b6e89fc80c89965aff1f7
L666F <fiorani.luca1998@gmail.com>
2018-05-24 19:30:12 +0200

    AutoMapper

---
+++ Controllers/Resources/CarModelResource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using vega.Models;
+
+namespace vega.Controllers.Resources
+{
+    public class CarModelResource
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Controllers/Resources/MakeResource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using vega.Models;
+
+namespace vega.Controllers.Resources
+{
+    public class MakeResource
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<CarModelResource> Models { get; set; }
+
+        public MakeResource() => Models = new List<CarModelResource>();
+    }
+}
