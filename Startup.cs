3d82da0f232c3ad8bd6c5acbeb0b88de9a664957
L666F <fiorani.luca1998@gmail.com>
2018-05-24 11:16:54 +0200

    Ignore

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,6 @@ namespace vega
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            //lol
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
